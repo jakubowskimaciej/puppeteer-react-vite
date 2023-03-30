19948de8df7f701f85fd9661c299e6d8fe3539de
Maciej Jakubowski <maciejjakubowski@Maciejs-MacBook-Air.local>
2023-03-30 15:41:08 +0200

    full size screenshot

--- server/server.js
+++ server/server.js
@@ -33,7 +33,10 @@ io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
   socket.on("browse", async () => {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({
+      headless: true,
+      defaultViewport: { width: 1920, height: 1080 },
+    });
     const page = await browser.newPage();
     await page.goto("https://github.com/login", {
       waitUntil: "load",
